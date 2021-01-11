9639682b08d5569e1e8c585fc617c0d6e768b754
Neil Gopal <gopaln@purdue.edu>
2021-01-10 21:02:55 -0800

    Added convert and download url paths

--- slowreverb/main/urls.py
+++ slowreverb/main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('convert', views.home, name='home'),
+    path('download', views.home, name='home'),
     path('', views.home, name='home'),
 ]
