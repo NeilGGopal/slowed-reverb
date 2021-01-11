6d683d5077ab4f22289837ea9767d3b97df9969d
Neil Gopal <gopaln@purdue.edu>
2021-01-11 09:59:34 -0800

    Export audio files to audio folder

--- slowreverb/main/views.py
+++ slowreverb/main/views.py
@@ -19,7 +19,7 @@ def home(request):
 
     mp4_audio = AudioSegment.from_file(f'/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.mp4', "mp4")
     # slowed_audio = mp4_audio.low_pass_filter(800)
-    mp4_audio.export(f"{url[32:]}.wav", format="wav")
+    mp4_audio.export(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.wav", format="wav")
 
     fx = (
         AudioEffectsChain()
@@ -27,7 +27,8 @@ def home(request):
         .speed(0.8)
     )
 
-    fx(f"{url[32:]}.wav", f"res-{url[32:]}.wav")
+    fx(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.wav", 
+    f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:]}.wav")
 
     context['URL'] = "DOWNLOAD"    
     return render(request, 'home.html', context)
