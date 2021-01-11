7fba8d1857427e87ba1634dafd27e8a4bb8caef9
Neil Gopal <gopaln@purdue.edu>
2021-01-11 12:20:54 -0800

    Added downloadable audio and media folder

--- slowreverb/slowreverb/settings.py
+++ slowreverb/slowreverb/settings.py
@@ -33,7 +33,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = [
-    '/Users/neilgopal/slowed-reverb/slowreverb/media'
+    '/Users/neilgopal/slowed-reverb/slowreverb/static/audio',
 ]
 
 # Application definition
@@ -126,4 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/audio/'
--- slowreverb/slowreverb/urls.py
+++ slowreverb/slowreverb/urls.py
@@ -22,4 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += staticfiles_urlpatterns()
