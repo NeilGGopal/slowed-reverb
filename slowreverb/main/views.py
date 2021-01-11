acc0920545be61444c726a10d34e174ed6628649
Neil Gopal <gopaln@purdue.edu>
2021-01-11 15:33:40 -0800

    Fixed ending slash bug and modified homepage

--- slowreverb/main/views.py
+++ slowreverb/main/views.py
@@ -19,14 +19,14 @@ def home(request, option):
     if url == '' or url is None:
         return render(request, 'home.html')
 
-    path = f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.wav"    
+    path = f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.wav"    
     
     if "convert" == option:
         yt = pytube.YouTube(url)
         audio = yt.streams.filter(only_audio=True).first()
-        audio.download('/Users/neilgopal/slowed-reverb/slowreverb/static/audio', url[32:]) 
+        audio.download('/Users/neilgopal/slowed-reverb/slowreverb/static/audio', url[32:43]) 
 
-        mp4_audio = AudioSegment.from_file(f'/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.mp4', "mp4")
+        mp4_audio = AudioSegment.from_file(f'/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.mp4', "mp4")
         # slowed_audio = mp4_audio.low_pass_filter(800)
         mp4_audio.export(path, format="wav")
 
@@ -36,10 +36,10 @@ def home(request, option):
             .speed(0.8)
         )
 
-        fx(path, f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:]}.wav")
+        fx(path, f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:43]}.wav")
 
     elif "download" in url_name:
         pass
 
-    context['URL'] = f"res-{url[32:]}.wav"
+    context['URL'] = f"res-{url[32:43]}.wav"
     return render(request, 'home.html', context)
