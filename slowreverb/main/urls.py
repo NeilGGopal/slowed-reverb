911c6e785f8a2b32128a1ff7c58014914f1a5e82
Neil Gopal <gopaln@purdue.edu>
2021-01-11 10:21:37 -0800

    Separated download and convert with req parameter

--- slowreverb/main/urls.py
+++ slowreverb/main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('convert', views.home, name='home'),
-    path('download', views.home, name='home'),
-    path('', views.home, name='home'),
+    path('convert', views.home, {'option': 'convert'}, name='home'),
+    path('download', views.home, {'option': 'download'}, name='home'),
+    path('', views.home, {'option': ''}, name='home'),
 ]
--- slowreverb/main/views.py
+++ slowreverb/main/views.py
@@ -6,29 +6,33 @@ from pysndfx import AudioEffectsChain
 
 # Create your views here.
 
-def home(request):
+def home(request, option):
     context = {}
     url = request.GET.get('URL')
+    url_name = request.resolver_match.view_name
 
     if url == '' or url is None:
         return render(request, 'home.html')
     
-    yt = pytube.YouTube(url)
-    audio = yt.streams.filter(only_audio=True).first()
-    audio.download('/Users/neilgopal/slowed-reverb/slowreverb/static/audio', url[32:]) 
+    if "convert" == option:
+        yt = pytube.YouTube(url)
+        audio = yt.streams.filter(only_audio=True).first()
+        audio.download('/Users/neilgopal/slowed-reverb/slowreverb/static/audio', url[32:]) 
 
-    mp4_audio = AudioSegment.from_file(f'/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.mp4', "mp4")
-    # slowed_audio = mp4_audio.low_pass_filter(800)
-    mp4_audio.export(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.wav", format="wav")
+        mp4_audio = AudioSegment.from_file(f'/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.mp4', "mp4")
+        # slowed_audio = mp4_audio.low_pass_filter(800)
+        mp4_audio.export(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.wav", format="wav")
 
-    fx = (
-        AudioEffectsChain()
-        .reverb()
-        .speed(0.8)
-    )
+        fx = (
+            AudioEffectsChain()
+            .reverb()
+            .speed(0.8)
+        )
 
-    fx(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.wav", 
-    f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:]}.wav")
+        fx(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:]}.wav", 
+        f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:]}.wav")
+    elif "download" in url_name:
+        pass
 
     context['URL'] = "DOWNLOAD"    
     return render(request, 'home.html', context)
