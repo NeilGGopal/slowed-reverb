6f5c3ac3db652badcbe59b37660a56234fbd244a
Neil Gopal <gopaln@purdue.edu>
2021-01-11 16:01:27 -0800

    Commented views.py file

--- slowreverb/main/views.py
+++ slowreverb/main/views.py
@@ -2,25 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 import pytube
-import os
 from pydub import AudioSegment
 from pysndfx import AudioEffectsChain
-import mimetypes
-from wsgiref.util import FileWrapper
 
 # Create your views here.
 
+# Homepage for the application
 def home(request, option):
-    context = {}
+    context = {}    # parameters to be sent to HTML
     context['URL'] = ""
     url = request.GET.get('URL')
     url_name = request.resolver_match.view_name
 
     if url == '' or url is None:
-        return render(request, 'home.html')
+        return render(request, 'home.html') # main page without audio
 
-    path = f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.wav"    
+    path = f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.wav" # audio path    
     
+    # if user chose conversion option
     if "convert" == option:
         yt = pytube.YouTube(url)
         audio = yt.streams.filter(only_audio=True).first()
@@ -34,12 +33,9 @@ def home(request, option):
             AudioEffectsChain()
             .reverb()
             .speed(0.8)
-        )
+        )   # applies effeects
 
-        fx(path, f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:43]}.wav")
-
-    elif "download" in url_name:
-        pass
+        fx(path, f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:43]}.wav")    # final file is downloaded
 
     context['URL'] = f"res-{url[32:43]}.wav"
-    return render(request, 'home.html', context)
+    return render(request, 'home.html', context)    # converted audio displayed on page
