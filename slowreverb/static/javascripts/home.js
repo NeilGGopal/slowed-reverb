9eea84699ed44a9ff51541d07c1283c15466a8f9
Neil Gopal <gopaln@purdue.edu>
2021-01-12 11:30:32 -0800

    Added progress bar for conversion

--- slowreverb/static/javascripts/home.js
+++ slowreverb/static/javascripts/home.js
@@ -1,6 +1,7 @@
 var convertBtn = document.querySelector('.convert-button'); // convert button
 var URLinput = document.querySelector('.url-input');        // url input
 var patientTxt = document.getElementById("patient");        // text telling user to be patient
+var progress = document.getElementById("myProgress");       // progress bar
 var url = document.getElementById("URL").value;             // url value from python parameters
 var audio = document.getElementById("audio");               // audio element
 
@@ -12,8 +13,31 @@ if (String(url).includes("mp3")) {
 convertBtn.addEventListener('click', () => {
     sendURL(URLinput.value);                                // calls sendURL() to change url
     patientTxt.style.visibility = "visible";                // makes patient text visible
+    progress.style.visibility = "visible";                  // makes progress bar visible
+    move();
 });
 
 function sendURL(URL) {
     window.location.href = `http://localhost:8000/convert?URL=${URL}`;  // changes url to conversion url
+}
+
+var i = 0;  // variable for width of inner progress bar
+// move progress bar
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width += 0.2;
+        elem.style.width = width + "%";
+        elem.innerHTML = Math.round(width) + "%";
+      }
+    }
+  }
 }
