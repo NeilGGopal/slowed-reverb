7286f48366f512c34270ecd687e47aaea3f7f932
Neil Gopal <gopaln@purdue.edu>
2021-06-04 14:12:25 -0700

    Fixed pytube HTTPS error

--- slowreverb/main/views.py
+++ slowreverb/main/views.py
@@ -6,10 +6,13 @@ from pysndfx import AudioEffectsChain
 import pytube
 import os
 import moviepy.editor as mp
+import time
 
 # Create your views here.
 
 # Homepage for the application
+
+
 def home(request, option):
     context = {}    # parameters to be sent to HTML
     context['URL'] = ""
@@ -17,26 +20,34 @@ def home(request, option):
     url_name = request.resolver_match.view_name
 
     if url == '' or url is None:
-        return render(request, 'home.html') # main page without audio
+        return render(request, 'home.html')  # main page without audio
 
-    parent_dir = '/Users/neilgopal/slowed-reverb/slowreverb/static/audio'             # file path
-    path = f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.mp4" # audio path
-    new_path = f'{parent_dir}/{url[32:43]}.mp3'                                       # mp3 result path
-    
+    # file path
+    parent_dir = '/Users/neilgopal/slowed-reverb/slowreverb/static/audio'             
+    # audio path
+    path = f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.mp4"
+    # mp3 result path
+    new_path = f'{parent_dir}/{url[32:43]}.mp3'
+
     if "download" == option:
         try:
-            os.remove(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:43]}.mp3") # remove downloaded file 
+            # remove downloaded file
+            time.sleep(0.5)
+            os.remove(
+                f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:43]}.mp3")
         except FileNotFoundError:
-            return render(request, 'home.html', context)    # converted audio displayed on page   
+            return render(request, 'home.html', context)
         context['URL'] = f"res-{url[32:43]}.mp3"
-        return render(request, 'home.html', context)    # converted audio displayed on page   
+        # converted audio displayed on page
+        return render(request, 'home.html', context)
 
     # if user chose conversion option
     if "convert" == option:
         if os.path.exists(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:43]}.mp3"):
             context['URL'] = f"res-{url[32:43]}.mp3"
-            return render(request, 'home.html', context)    # converted audio displayed on page
-            
+            # converted audio displayed on page
+            return render(request, 'home.html', context)
+
         yt = pytube.YouTube(url)
         audio = yt.streams.filter(only_audio=True, file_extension='mp4').first()
         audio.download(parent_dir, url[32:43])      # downloads file to parent_dir as mp4
@@ -51,12 +62,17 @@ def home(request, option):
             .speed(0.8)
         )   # applies effects
 
-        fx(new_path, f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:43]}.mp3")    # final file is downloaded
+        # final file is downloaded
+        fx(new_path,
+           f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/res-{url[32:43]}.mp3")
         try:
-            os.remove(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.mp3")
-            os.remove(f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.mp4") 
+            os.remove(
+                f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.mp3")
+            os.remove(
+                f"/Users/neilgopal/slowed-reverb/slowreverb/static/audio/{url[32:43]}.mp4")
         except FileNotFoundError:
-            pass    
-    
+            pass
+
     context['URL'] = f"res-{url[32:43]}.mp3"
-    return render(request, 'home.html', context)    # converted audio displayed on page
+    # converted audio displayed on page
+    return render(request, 'home.html', context)
--- slowreverb/static/javascripts/home.js
+++ slowreverb/static/javascripts/home.js
@@ -1,50 +1,55 @@
-var convertBtn = document.querySelector('.convert-button'); // convert button
-var URLinput = document.querySelector('.url-input');        // url input
-var convertTxt = document.getElementById("converting");     // text informing user of conversion process
-var embedDiv = document.getElementById("embedDiv");         // div element to store convert text and bar
-var progress = document.getElementById("myProgress");       // progress bar
-var url = document.getElementById("URL").value;             // url value from python parameters
-var audio = document.getElementById("audio");               // audio element
+var convertBtn = document.querySelector(".convert-button"); // convert button
+var URLinput = document.querySelector(".url-input"); // url input
+var convertTxt = document.getElementById("converting"); // text informing user of conversion process
+var embedDiv = document.getElementById("embedDiv"); // div element to store convert text and bar
+var progress = document.getElementById("myProgress"); // progress bar
+var url = document.getElementById("URL").value; // url value from python parameters
+var audio = document.getElementById("audio"); // audio element
 
-if (!window.location.href.includes('convert')) {
-    audio.style.visibility = "hidden";
-    audio.setAttribute('src', '');
-}   // sets audio to invisible on homepage
+if (!window.location.href.includes("convert")) {
+  audio.style.visibility = "hidden";
+  audio.setAttribute("src", "");
+} // sets audio to invisible on homepage
 
 // checks if user has converted link correctly
-if (String(url).includes("mp3") && !String(window.location.href).includes("download")) {
-    var link = document.getElementById("download");         // a tag for automatic download
-    link.setAttribute("href", `/static/audio/${url}/`)
-    link.setAttribute("download", `/static/audio/${url}/`);
-    link.click();                                           // clicks a tag that forces download
-    // audio.setAttribute('src', `/static/audio/${url}/`)   // changes audio element to reference converted file
-    window.location.href = 
-    `${window.location.href.substring(0,22)}download${window.location.href.substring(29)}`; // changes url to download url
+if (
+  String(url).includes("mp3") &&
+  !String(window.location.href).includes("download")
+) {
+  var link = document.getElementById("download"); // a tag for automatic download
+  link.setAttribute("href", `/static/audio/${url}/`);
+  link.setAttribute("download", `/static/audio/${url}/`);
+  link.click(); // clicks a tag that forces download
+  // audio.setAttribute('src', `/static/audio/${url}/`)   // changes audio element to reference converted file
+  window.location.href = `${window.location.href.substring(
+    0,
+    22
+  )}download${window.location.href.substring(29)}`; // changes url to download url
 }
 
 // checks if user click conversion button
-convertBtn.addEventListener('click', () => {
-    if (!URLinput.value.includes("youtube.com/watch?v=")) {
-        return;
-    }                                                       // checks if URL input is a YouTube link
-    sendURL(URLinput.value);                                // calls sendURL() to change url
-    
-    audio.style.visibility = "hidden";                      //
-    audio.setAttribute('src', '');                          // hides audio if conversion clicked on convert page
-    embedDiv.appendChild(convertTxt);                       // moves conversion text above audio
-    embedDiv.appendChild(progress);                         // moves progress bar above audio
+convertBtn.addEventListener("click", () => {
+  if (!URLinput.value.includes("youtube.com/watch?v=")) {
+    return;
+  } // checks if URL input is a YouTube link
+  sendURL(URLinput.value); // calls sendURL() to change url
 
-    convertTxt.style.visibility = "visible";                // makes convert text visible
-    progress.style.visibility = "visible";                  // makes progress bar visible
-    move();
+  audio.style.visibility = "hidden"; //
+  audio.setAttribute("src", ""); // hides audio if conversion clicked on convert page
+  embedDiv.appendChild(convertTxt); // moves conversion text above audio
+  embedDiv.appendChild(progress); // moves progress bar above audio
+
+  convertTxt.style.visibility = "visible"; // makes convert text visible
+  progress.style.visibility = "visible"; // makes progress bar visible
+  move();
 });
 
 // changes url to conversion url
 function sendURL(URL) {
-    window.location.href = `http://localhost:8000/convert?URL=${URL}`;  // changes url to conversion url
+  window.location.href = `http://localhost:8000/convert?URL=${URL}`; // changes url to conversion url
 }
 
-var i = 0;  // variable for width of inner progress bar
+var i = 0; // variable for width of inner progress bar
 // move progress bar
 function move() {
   if (i == 0) {
